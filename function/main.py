f4ebc14d030832be79bddffb2f234892f9a49ca0
facuv <vfacuu@gmail.com>
2024-07-25 17:19:50 -0300

    added threads to etl script

--- function/main.py
+++ function/main.py
@@ -4,17 +4,15 @@ import os
 from shapely import wkt
 import geopandas as gpd
 from datetime import datetime
+import pandas as pd
+from threading import Thread
 
 load_dotenv()
 
-def ingest_trips( file='trips.csv' ):
-    params = os.environ
-
-    # Connect to postgis
-    engine = create_engine("postgresql+psycopg2://"+ params['DB_USER'] +":"+ params['DB_PASS']+ "@localhost:5432/test_db")
-
+def tripETL( df, engine, i):
+    print(f"starting thread {i}")
     # Extract
-    gdf = gpd.read_file( file )
+    gdf = gpd.GeoDataFrame( df )
 
     # Transform coordinates into geometry objects
     gdf['origin_coord'] = gdf['origin_coord'].apply(wkt.loads)
@@ -24,7 +22,25 @@ def ingest_trips( file='trips.csv' ):
     gdf['week_no'] = gdf['datetime'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime("%Y%V") )
 
     # Append data into postgis
-    gdf.to_postgis( params['TABLE_NAME'] ,if_exists='append', con=engine)  
+    gdf.to_postgis( 'trips' ,if_exists='append', con=engine, chunksize=100000) 
+    print(f"thread {i} finished")
+
+
+def ingest_trips( file='trips.csv' ):
+    params = os.environ
+    # Connect to postgis
+    engine = create_engine("postgresql+psycopg2://"+ params['DB_USER'] +":"+ params['DB_PASS']+ "@localhost:5432/test_db")
+
+    threads = []
+    reader = pd.read_csv(file, chunksize=100000) 
+    i=0
+    for chunk in reader:
+        t = Thread(target=tripETL, args=(chunk,engine,i,))
+        i+=1
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join() 
 
 
 def weekly_avg_by_region( region, engine ):
@@ -48,4 +64,3 @@ def weekly_avg_by_box( xmin, ymin, xmax, ymax, engine ):
     with engine.connect() as connection:
         result = connection.execute(text(query))
     return result.fetchone().t[0]
-
