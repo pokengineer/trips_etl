e38b36b8729c4316a67c100e76720053eb1c70c1
facuv <vfacuu@gmail.com>
2024-07-24 23:26:40 -0300

    added requirements.txt

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Data Engineering Challenge
 The task is to build an automatic process to ingest data on an on-demand basis. The data represents trips taken by different vehicles, and include a city, a point of origin and a destination.
 
+# install necesary requirements
+Before running any code it is important to install libraries used in the project. You can install them using pip:
+> pip install -r requirements.txt
+
 # create the database
 Mount Docker image for the development database, run the following commands to start the docker image for the database
 
->cd setup
->docker-compose up -d
->python setup.py
+>cd setup <br>
+>docker-compose up -d <br>
+>python setup.py <br>
+
+
--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from dotenv import load_dotenv
 import os
 from shapely import wkt
 import geopandas as gpd
+from datetime import datetime
+
+load_dotenv()
 
 def ingest_trips( file='trips.csv' ):
-    load_dotenv()
     params = os.environ
 
     # Connect to postgis
@@ -19,6 +21,9 @@ def ingest_trips( file='trips.csv' ):
     gdf = gdf.set_geometry('origin_coord')
     gdf['destination_coord'] = gdf['destination_coord'].apply(wkt.loads)
     gdf = gdf.set_geometry('destination_coord')
+    gdf['datetime'] = gdf['datetime'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime("%Y%V") )
 
     # Append data into postgis
     gdf.to_postgis( params['TABLE_NAME'] ,if_exists='append', con=engine)  
+
+    
