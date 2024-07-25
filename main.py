df468de152dd3b1b6590db88e1f02aee01510ae2
facuv <vfacuu@gmail.com>
2024-07-24 23:41:04 -0300

    added functions for weekly average number of trips

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 from dotenv import load_dotenv
 import os
 from shapely import wkt
@@ -21,9 +21,31 @@ def ingest_trips( file='trips.csv' ):
     gdf = gdf.set_geometry('origin_coord')
     gdf['destination_coord'] = gdf['destination_coord'].apply(wkt.loads)
     gdf = gdf.set_geometry('destination_coord')
-    gdf['datetime'] = gdf['datetime'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime("%Y%V") )
+    gdf['week_no'] = gdf['datetime'].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S").strftime("%Y%V") )
 
     # Append data into postgis
     gdf.to_postgis( params['TABLE_NAME'] ,if_exists='append', con=engine)  
 
-    
+
+def weekly_avg_by_region( region, engine ):
+    query = """select avg(no_trips)
+        from ( select 
+        week_no, count(id) as no_trips
+        from trips t 
+        where region = '""" + str(region) + """'
+        group by week_no )"""
+    with engine.connect() as connection:
+        result = connection.execute(text(query))
+    return result.fetchone().t[0]
+
+def weekly_avg_by_box( xmin, ymin, xmax, ymax, engine ):
+    query = """select avg(no_trips)
+        from ( select 
+        week_no, count(id) as no_trips
+        from trips t 
+        where destination_coord && ST_MakeEnvelope( """ + ",".join([str(x) for x in [xmin, ymin, xmax, ymax]]) + """)
+        group by week_no )"""
+    with engine.connect() as connection:
+        result = connection.execute(text(query))
+    return result.fetchone().t[0]
+
